a01cc65c565ab51f6aea6a61beab670536fa23a1
vhreptovich <vhreptovich@gmail.com>
2023-12-29 20:02:42 +0300

    Fix

--- counter.go
+++ counter.go
@@ -1,3 +1,6 @@
+// This is a PoC/illustrative code to show how to share a single integer that goes up in k6 on a
+// single instance
+
 package counter
 
 import (
@@ -10,12 +13,36 @@ func init() {
 	modules.Register("k6/x/counter", New())
 }
 
-var realCounter int64
+var reqCounter int64
+var successCounter int64
+var failedCounter int64
 
 type counter struct{}
 
-func (c *counter) Up() int64 {
-	return atomic.AddInt64(&realCounter, 1)
+func (c *counter) Success() int64 {
+	atomic.AddInt64(&reqCounter, 1)
+	return atomic.AddInt64(&successCounter, 1)
+}
+
+func (c *counter) Fail() int64 {
+	atomic.AddInt64(&reqCounter, 1)
+	return atomic.AddInt64(&failedCounter, 1)
+}
+
+func (c *counter) SuccessCount() int64 {
+	return atomic.LoadInt64(&successCounter)
+}
+
+func (c *counter) ErrorCount() int64 {
+	return atomic.LoadInt64(&failedCounter)
+}
+
+func (c *counter) SuccessRate() int64 {
+	return atomic.LoadInt64(&successCounter) * 100 / atomic.LoadInt64(&reqCounter)
+}
+
+func (c *counter) ErrorRate() int64 {
+	return atomic.LoadInt64(&failedCounter) * 100 / atomic.LoadInt64(&reqCounter)
 }
 
 func New() *counter {
