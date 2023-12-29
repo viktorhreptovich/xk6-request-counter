fca5428c915c51b8901f1e1d50b6bfd8436c1d1a
vhreptovich <vhreptovich@gmail.com>
2023-12-29 19:55:59 +0300

    Fix

--- counter.go
+++ counter.go
@@ -20,29 +20,11 @@ var failedCounter int64
 type counter struct{}
 
 func (c *counter) Success() int64 {
-	atomic.AddInt64(&reqCounter, 1)
 	return atomic.AddInt64(&successCounter, 1)
-
 }
+
 func (c *counter) Fail() int64 {
-	atomic.AddInt64(&reqCounter, 1)
 	return atomic.AddInt64(&failedCounter, 1)
-}
-
-func (c *counter) SuccessCount() int64 {
-	return atomic.LoadInt64(&successCounter)
-}
-
-func (c *counter) ErrorCount() int64 {
-	return atomic.LoadInt64(&failedCounter)
-}
-
-func (c *counter) SuccessRate() int64 {
-	return atomic.LoadInt64(&successCounter) * 100 / atomic.LoadInt64(&reqCounter)
-}
-
-func (c *counter) ErrorRate() int64 {
-	return atomic.LoadInt64(&failedCounter) * 100 / atomic.LoadInt64(&reqCounter)
 }
 
 func New() *counter {
